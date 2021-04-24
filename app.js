bc4074cae13ad4fb80d4789808c87ba36e3d4051
Alekh <alekhkumar777@gmail.com>
2021-04-24 11:29:30 +0530

    Add input from textfield

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 let idCount = 1;
 btn_add.onclick = function () {
-    let div2 = document.createElement("div");
-  div2.appendChild(document.createTextNode("this is test"));
+  let div2 = document.createElement("div");
+  let txt = document.getElementById('inp_task').value;
+  console.log(txt);
+  div2.appendChild(document.createTextNode(txt));
   div2.setAttribute('id', "task"+idCount);
   idCount++;
   let div1 = document.getElementById("div1");
   div1.appendChild(div2);
   
-};
-
-btn_del.onclick = function () {
-    document.getElementById("task"+ --idCount).remove();
 };
