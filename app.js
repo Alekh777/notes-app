b2d626772c1b06cbf1f91f91ada33ec5a42827c9
Alekh <alekhkumar777@gmail.com>
2021-04-24 12:52:12 +0530

    Add enter key press for button

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 let idCount = 1;
+let txt = document.getElementById("inp_task");
+
+txt.addEventListener("keyup", function(event){
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("btn_add").click();
+  }
+});
+
 btn_add.onclick = function () {
+  if(txt.value == "")
+    return;
   let div2 = document.createElement("div");
-  let txt = document.getElementById('inp_task').value;
-  console.log(txt);
-  div2.appendChild(document.createTextNode(txt));
-  div2.setAttribute('id', "task"+idCount);
+  console.log(txt.value);
+  div2.innerHTML = "<h3>" + txt.value + "</h3>";
+  div2.setAttribute("id", "task" + idCount);
+  div2.setAttribute("class", "task");
   idCount++;
   let div1 = document.getElementById("div1");
-  div1.appendChild(div2);
-  
+  div1.insertBefore(div2, div1.firstElementChild);
+  txt.value = "";
 };
